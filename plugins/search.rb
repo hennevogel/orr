4ccb29e6cf7efbcd92051217999fc23f1b2f6e87
Cornelius Schumacher <cschum@suse.com>
2017-07-17 17:49:31 +0200

    Implement search
    This assumes that repos with available ruby packages have already
    been set up. Future extension would be to add the required repos
    (depends on distribution).

--- plugins/search.rb
+++ plugins/search.rb
@@ -1,5 +1,27 @@
+require_relative "../lib/shell_command"
+require "rexml/document"
+
 class OrrSearchCommand < Clamp::Command
   def execute
-    puts "search"
+    rubies = []
+
+    xml = shell_command.run("zypper --xml se 'ruby*'")
+    doc = REXML::Document.new(xml)
+    doc.elements.each("stream/search-result/solvable-list/solvable") do |element|
+      name = element.attributes["name"]
+      status = element.attributes["status"]
+      kind = element.attributes["kind"]
+      if name =~ /^ruby\d\.\d+$/ && kind == "package"
+        ruby = name
+        ruby += " (installed)" if status == "installed"
+        rubies.push(ruby)
+      end
+    end
+
+    puts rubies.join("\n")
+  end
+
+  def shell_command
+    ShellCommand.new
   end
 end

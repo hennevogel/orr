5f9dcf1ce6c9d7bc8231566531e81e9e6eaec443
Cornelius Schumacher <cschum@suse.com>
2017-07-19 16:17:02 +0200

    Implement list of installed rubies

--- plugins/list.rb
+++ plugins/list.rb
@@ -1,5 +1,28 @@
 class OrrListCommand < Clamp::Command
   def execute
-    puts "list"
+    puts "Installed ruby versions:"
+    rpms = shell_command.run("rpm -qa")
+    rubies = []
+    rpms.each_line do |rpm|
+      if rpm =~ /^(ruby\d.\d+)-\d/
+        rubies.push $1
+      end
+    end
+    rubies.sort.each do |ruby|
+      puts "* #{ruby}"
+    end
+    puts
+
+    puts "Active ruby:"
+    ruby_path = shell_command.run("which ruby")
+    ruby_rpm = shell_command.run("rpm -qf #{ruby_path}")
+    ruby_rpm =~ /^(ruby\d\.\d+)-/
+    ruby_name = $1
+    ruby_version = shell_command.run("ruby -v")
+    puts "* #{ruby_name} (path: #{ruby_path}, version: #{ruby_version})"
+  end
+
+  def shell_command
+    ShellCommand.new
   end
 end

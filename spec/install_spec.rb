a584a648c0a575e74bc57d900576c99bf48c81ab
Henne Vogelsang <hvogel@opensuse.org>
2017-07-27 13:08:14 +0200

    Adopt the test suite to the OrrConfig changes

--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -8,106 +8,58 @@ describe OrrInstallCommand do
   end
 
   describe "when ruby version is not installed yet" do
-    it "installs given ruby version" do
-      arguments = ["2.3"]
-      Dir.mktmpdir("orr-test") do |home_dir|
-        bin_dir = Pathname.new(home_dir) + "bin"
-        Dir.mkdir(bin_dir)
-
-        bashrc = Pathname.new(home_dir) + ".bashrc"
-        bashrc_content = <<EOT
-# This is an exemplary .bashrc
-PS1='something'
-EOT
-        File.write(bashrc, bashrc_content)
-
-        env = ENV.to_hash.merge("HOME" => home_dir)
-
-        mock = Minitest::Mock.new
-        mock.expect :run_interactive, nil, ["sudo zypper in ruby2.3"]
-
-        @cmd.stub :shell_command, mock do
-          assert_output(/^Installing ruby2.3 in #{bin_dir}\n/) do
-            Object.stub_const(:ENV, env) do
-              @cmd.run(arguments)
-            end
-          end
-        end
-        assert_equal("/usr/bin/ruby.ruby2.3", File.readlink(bin_dir + "ruby"))
-        assert_equal("/usr/bin/irb.ruby2.3", File.readlink(bin_dir + "irb"))
-        assert_equal("/usr/bin/gem.ruby2.3", File.readlink(bin_dir + "gem"))
-        assert_equal("/usr/bin/rake.ruby2.3", File.readlink(bin_dir + "rake"))
-
-        expected_bashrc_content = bashrc_content + <<EOT
+    before do
+      @expected_profile_content = <<EOT
 export GEM_HOME=~/.gems/ruby2.3
 export PATH=$GEM_HOME/bin:$PATH
 EOT
-        assert_equal(expected_bashrc_content, File.read(bashrc))
+    end
+    it "installs given ruby version" do
+      arguments = ["2.3"]
+      mock = Minitest::Mock.new
+      mock.expect :run_interactive, nil, ["sudo zypper in ruby2.3"]
 
-        expected_gemrc_content = <<EOT
-install: --no-format-executable
-EOT
-        assert_equal(expected_gemrc_content, File.read(Pathname.new(home_dir) + ".gemrc"))
+      @cmd.stub :shell_command, mock do
+        assert_output(/^Installing ruby2.3 in #{BIN_DIR}\n/) do
+          @cmd.run(arguments)
+        end
       end
+
+      ORR_BINARIES.each do |cmd|
+        assert_equal("/usr/bin/#{cmd}.ruby2.3", File.readlink(BIN_DIR + cmd))
+      end
+
+      assert_equal(@expected_profile_content, File.read(PROFILE_FILE))
     end
   end
 
   describe "when ruby version is already installed" do
-    it "installs given ruby version" do
-      arguments = ["2.3"]
-      Dir.mktmpdir("orr-test") do |home_dir|
-        bin_dir = Pathname.new(home_dir) + "bin"
-        Dir.mkdir(bin_dir)
-        ["ruby", "irb", "gem", "rake"].each do |cmd|
-          File.symlink("/usr/bin/#{cmd}.ruby2.2", bin_dir + cmd)
-        end
-
-        bashrc = Pathname.new(home_dir) + ".bashrc"
-        bashrc_content = <<EOT
-# This is an exemplary .bashrc
-PS1='something'
-export GEM_HOME=~/.gems/ruby2.2
-export PATH=$GEM_HOME/bin:$PATH
-# something else
-EOT
-        File.write(bashrc, bashrc_content)
-
-        gemrc_content = <<EOT
-install: --no-format-executable
-EOT
-        File.write(Pathname(home_dir) + ".gemrc", gemrc_content)
-
-        env = ENV.to_hash.merge("HOME" => home_dir)
-
-        mock = Minitest::Mock.new
-        mock.expect :run_interactive, nil, ["sudo zypper in ruby2.3"]
-
-        @cmd.stub :shell_command, mock do
-          assert_output(/^Installing ruby2.3 in #{bin_dir}\n/) do
-            Object.stub_const(:ENV, env) do
-              @cmd.run(arguments)
-            end
-          end
-        end
-        assert_equal("/usr/bin/ruby.ruby2.3", File.readlink(bin_dir + "ruby"))
-        assert_equal("/usr/bin/irb.ruby2.3", File.readlink(bin_dir + "irb"))
-        assert_equal("/usr/bin/gem.ruby2.3", File.readlink(bin_dir + "gem"))
-        assert_equal("/usr/bin/rake.ruby2.3", File.readlink(bin_dir + "rake"))
-
-        expected_bashrc_content = <<EOT
-# This is an exemplary .bashrc
-PS1='something'
-export GEM_HOME=~/.gems/ruby2.3
-export PATH=$GEM_HOME/bin:$PATH
-# something else
-EOT
-        assert_equal(expected_bashrc_content, File.read(bashrc))
-
-        expected_gemrc_content = <<EOT
-install: --no-format-executable
-EOT
-        assert_equal(expected_gemrc_content, File.read(Pathname.new(home_dir) + ".gemrc"))
+    before do
+      ORR_BINARIES.each do |cmd|
+        File.symlink("/usr/bin/#{cmd}.ruby2.2", BIN_DIR + cmd)
       end
+      @expected_profile_content = <<EOT
+export GEM_HOME=~/.gems/ruby2.4
+export PATH=$GEM_HOME/bin:$PATH
+EOT
+    end
+
+    it "installs given ruby version" do
+      arguments = ["2.4"]
+      mock = Minitest::Mock.new
+      mock.expect :run_interactive, nil, ["sudo zypper in ruby2.4"]
+
+      @cmd.stub :shell_command, mock do
+        assert_output(/^Installing ruby2.4 in #{BIN_DIR}\n/) do
+          @cmd.run(arguments)
+        end
+      end
+
+      ORR_BINARIES.each do |cmd|
+        assert_equal("/usr/bin/#{cmd}.ruby2.4", File.readlink(BIN_DIR + cmd))
+      end
+
+      assert_equal(@expected_profile_content, File.read(PROFILE_FILE))
     end
   end
 end

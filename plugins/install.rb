fae7be63da992b6b0ebc5b3d3c3697391d020730
Henne Vogelsang <hvogel@opensuse.org>
2017-11-14 16:29:19 +0100

    First shot at the use command

--- plugins/install.rb
+++ plugins/install.rb
@@ -9,24 +9,6 @@ class OrrInstallCommand < OrrCommand
     success = shell_command.run_interactive("sudo zypper in -f ruby#{ruby_version}")
     return unless success
 
-    FileUtils.cd(BIN_DIR) do
-      FileUtils.rm_f ORR_BINARIES
-    end
-    ORR_BINARIES.each do |cmd|
-      File.symlink("/usr/bin/#{cmd}.ruby#{ruby_version}", BIN_DIR + cmd)
-    end
-
-    replace_profile(ruby_version)
-    puts "Make sure to restart your shell to make new GEM_HOME and PATH settings active"
-  end
-
-  def replace_profile(ruby_version)
-    export_gem_home = "export GEM_HOME=~/.gems/ruby#{ruby_version}"
-    export_path = "export PATH=#{BIN_DIR}:$GEM_HOME/bin:$PATH"
-
-    File.open(PROFILE_FILE, 'w') do |f|
-      f.puts(export_gem_home)
-      f.puts(export_path)
-    end
+    OrrUseCommand.new('').run([ruby_version])
   end
 end
---
+++ spec/use_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'test_helper'
+require_relative '../plugins/' + File.basename(__FILE__, '_spec.rb')
+require 'fileutils'
+
+describe OrrUseCommand do
+  describe "when ruby version used" do
+    before do
+      @cmd = OrrUseCommand.new('')
+
+      ORR_BINARIES.each do |cmd|
+        File.symlink("/usr/bin/#{cmd}.ruby2.2", BIN_DIR + cmd)
+      end
+      FileUtils.rm_f(PROFILE_FILE)
+      @expected_profile_content = <<EOT
+export GEM_HOME=~/.gems/ruby2.5
+export PATH=#{BIN_DIR}:$GEM_HOME/bin:$PATH
+EOT
+    end
+
+    it "uses given ruby version" do
+      @cmd.run(['2.5'])
+      ORR_BINARIES.each do |cmd|
+        assert_equal("/usr/bin/#{cmd}.ruby2.5", File.readlink(BIN_DIR + cmd))
+      end
+
+      assert_equal(@expected_profile_content, File.read(PROFILE_FILE))
+    end
+  end
+end

404613712e6931945d3339d2bfa12d37de8ca80c
Henne Vogelsang <hvogel@opensuse.org>
2017-11-15 14:43:36 +0100

    Introduce default versions

--- plugins/list.rb
+++ plugins/list.rb
@@ -4,27 +4,37 @@ class OrrListCommand < OrrCommand
     
     rubies = query_system
     rubies.sort.each do |ruby|
-      puts "* ruby#{ruby}"
+      puts "#{version_marker(ruby)} #{ruby}"
     end
+    puts
+    puts '# => - current'
+    puts '# =* - current && default'
+    puts '#  * - default'
     puts
 
     puts "Active ruby:"
     ruby_path = shell_command.run("which ruby")
     ruby_rpm = shell_command.run("rpm -qf #{ruby_path}")
-    ruby_rpm =~ /^(ruby\d\.\d+)-/
+    ruby_rpm =~ /^ruby(\d\.\d+)-/
     ruby_name = Regexp.last_match(1)
     ruby_version = shell_command.run("ruby -v")
     puts "* #{ruby_name} (path: #{ruby_path}, version: #{ruby_version})"
   end
 
   def query_system
-    rpms = shell_command.run("rpm -qa ruby\*")
+    rpms = shell_command.run("rpm -qa --qf '%{NAME}\n' ruby\*")
     rubies = []
     rpms.each_line do |rpm|
-      if rpm =~ /^ruby(\d.\d+)-\d/
+      if rpm =~ /^ruby(\d.\d+)$/
         rubies.push Regexp.last_match(1)
       end
     end
     rubies
+  end
+  def version_marker(ruby_version)
+    return "=*" if ENV['ORR_RUBY'] == ruby_version && SUPPORTED[DISTRO] == ruby_version
+    return ' *' if ENV['ORR_RUBY'] != ruby_version && SUPPORTED[DISTRO] == ruby_version
+    return '=>' if ENV['ORR_RUBY'] == ruby_version
+    return '  '
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -10,28 +10,31 @@ describe OrrInfoCommand do
   describe "when executed" do
     it "lists installed and active ruby versions" do
       rpm_out = <<EOT
-libruby2_2-2_2-2.2.5-1.5.x86_64
-ruby2.2-rubygem-rack-protection-1.5.3-4.1.x86_64
-ruby2.2-2.2.5-1.5.x86_64
-ruby2.2-rubygem-rack-1.6.4-22.21.x86_64
-ruby2.1-2.1.9-1.1.x86_64
-ruby2.2-rubygem-rspec-support-3.5.0-1.1.x86_64
-ruby-devel-2.2-1.3.x86_64
+ruby2.2-rubygem-rack-protection
+ruby2.2
+ruby2.2-rubygem-rack
+ruby2.1
+ruby2.2-rubygem-rspec-support
+ruby-devel
 EOT
 
       mock = Minitest::Mock.new
-      mock.expect :run, rpm_out, ["rpm -qa ruby\*"]
+      mock.expect :run, rpm_out, ["rpm -qa --qf '%{NAME}\n' ruby\*"]
       mock.expect :run, "/path/to/ruby", ["which ruby"]
       mock.expect :run, "ruby2.2-2.2.5-1.5.x86_64", ["rpm -qf /path/to/ruby"]
       mock.expect :run, "ruby 2.2.5p319 (2016-04-26 revision 54774) [x86_64-linux-gnu]", ["ruby -v"]
 
       expected_out = <<EOT
 Installed ruby versions:
-* ruby2.1
-* ruby2.2
+ * 2.1
+   2.2
+
+# => - current
+# =* - current && default
+#  * - default
 
 Active ruby:
-* ruby2.2 (path: /path/to/ruby, version: ruby 2.2.5p319 (2016-04-26 revision 54774) [x86_64-linux-gnu])
+* 2.2 (path: /path/to/ruby, version: ruby 2.2.5p319 (2016-04-26 revision 54774) [x86_64-linux-gnu])
 EOT
 
       @info.stub :shell_command, mock do
